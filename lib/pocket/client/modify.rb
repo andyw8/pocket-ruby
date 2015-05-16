923d006ed9c669550f4be4581e518901b911b4d2
Keith Webber <kwebber@backupify.com>
2015-05-16 00:15:28 -0400

    adjusted behavior of Modify#modify

--- lib/pocket/client/modify.rb
+++ lib/pocket/client/modify.rb
@@ -3,8 +3,8 @@ module Pocket
     # http://getpocket.com/developer/docs/v3/modify
     module Modify
       # required params: actions, consumer_key, access_token
-      def modify actions
-        response = connection.post("/v3/send", {actions: actions})
+      def modify actions, params
+        response = connection.post("/v3/send", {actions: actions}.merge(params))
         response.body
       end
     end
