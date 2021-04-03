acfa974f675c43ddea1dabab7a3463e51591a9b0
Andy Waite <andyw8@users.noreply.github.com>
2021-04-02 21:09:48 -0400

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start do
+  enable_coverage :branch
+end
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "pocket-ruby"
 
