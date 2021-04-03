d767d7ab2ecbcc0b3c8bdcc6046223c0c9b887b8
Andy Waite <andyw8@users.noreply.github.com>
2021-04-03 17:23:17 -0400

    Add support for authors and tags in `Pocket::Article` (#44)

---
+++ lib/pocket/author.rb
@@ -0,0 +1,23 @@
+module Pocket
+  class Author
+    def initialize(response)
+      @response = response
+    end
+
+    def id
+      Integer(response.fetch("author_id"))
+    end
+
+    def name
+      response.fetch("name")
+    end
+
+    def url
+      response.fetch("url")
+    end
+
+    private
+
+    attr_reader :response
+  end
+end
