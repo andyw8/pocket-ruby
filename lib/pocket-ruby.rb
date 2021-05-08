7e7875327afa91ded3c73c785cba9bc4d1ac3a26
Andy Waite <andyw8@users.noreply.github.com>
2021-05-08 17:03:31 -0400

    Add `Pocket::Image` class

--- lib/pocket-ruby.rb
+++ lib/pocket-ruby.rb
@@ -5,6 +5,7 @@ require File.expand_path("../pocket/client", __FILE__)
 require File.expand_path("../pocket/article", __FILE__)
 require File.expand_path("../pocket/author", __FILE__)
 require File.expand_path("../pocket/domain_metadata", __FILE__)
+require File.expand_path("../pocket/image", __FILE__)
 
 module Pocket
   extend Configuration
---
+++ lib/pocket/image.rb
@@ -0,0 +1,21 @@
+module Pocket
+  class Image
+    attr_reader :response
+
+    def initialize(response)
+      @response = response
+    end
+
+    def src
+      response.fetch("src")
+    end
+
+    def caption
+      response.fetch("caption")
+    end
+
+    def image_id
+      Integer(response.fetch("image_id"))
+    end
+  end
+end
